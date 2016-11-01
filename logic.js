4fc7242b67a967f5c1855058a7f8967320c6be33
Flarp <flurpdadurp@gmail.com>
2016-11-01 19:25:34 +0000

    Add more fluff

--- logic.js
+++ logic.js
@@ -13,15 +13,15 @@ const generate = () => {
             const split = json.nouns[Math.floor(Math.random() * json.nouns.length)].split('')
             split[0] = String.fromCharCode(split[0].charCodeAt(0) - 32)
             const noun = split.join('')
+            const licenses = ['MIT', 'BSD', 'GPL-3.0', 'MPL', 'Apache-2.0', 'GPL-3.0']
             console.log(noun)
             const data = ['JSON', 'XML']
             const final = `<center><h1>${noun}</h1></center>
             <br>
-            \t<p>${noun} is a new ${getRandom(language)} framework inspired by ${getRandom(based)}. This new library can be got from npm using -
+            \t<p>${noun} is a new ${getRandom(language)} MVC framework inspired by ${getRandom(based)}. This new library can be got from npm using - </p>
             <br>
             <br>
             <center><code>npm install ${noun.toLowerCase()}</code></center>
-            <br>
             <br>
             <br>
             <h2>Features</h2>
@@ -29,9 +29,58 @@ const generate = () => {
                 <li> <b>${getRandom(type)}</b>: Syntax recognized by many programmers makes it much easier to use, as many people are familiar with this kind of programming.</li>
                 <li> <b>It's Just JavaScript</b>: It's nothing more than simple JavaScript in the end, just makes everything a little easier. </li>
                 <li> <b>Small</b>: Is only ${Math.floor(Math.random() * 1000)}kb!</li>
-                <li> <b> Useful DOM</b>: The DOM is in ${getRandom(data)} format, so you don't need to learn a whole new kind of DSL! </li>
+                <li> <b>Useful DOM</b>: The DOM is in ${getRandom(data)} format, so you don't need to learn a whole new kind of DSL! </li>
             </ul>
-            </p>`
+            <br>
+            <br>
+            <h2>Example</h2>
+            <code>
+            <pre>
+            ${getRandom(data) === 'XML' 
+                ? `<script>
+                    var myArr = ["${noun} is cool!", "HTML5 is cooler!"]
+                </script>
+                
+                <div className="${noun}-body">
+                    <${noun}-header size="1">This is a test ${noun} header!</${noun}-header>
+                    <${noun}-paragraph>This is a test ${noun} paragraph!</${noun}-paragraph>
+                    <${noun}-map val="myVal" arr="myArr">
+                        <${noun}-paragraph>Array says {myVal}</${noun}-paragraph>
+                    </${noun}-map>
+            </div>`.split('<').join('&lt;').split('>').join('&gt;')
+                : `var myArr = ["${noun} is cool!", "HTML5 is cooler!"]
+                
+                new ${noun}.Element({
+                    type: 'body',
+                    content: [new ${noun}.Element({
+                        type: 'h1',
+                        options: {
+                            size: 1
+                        },
+                        content: This is a test ${noun} header!
+                    }),
+                    new ${noun}.Element({
+                        type: 'p',
+                        content: 'This is a test ${noun} paragraph!
+                    }),
+                    new ${noun}.Element({
+                        type: '${noun}-map'
+                        options: {
+                            val: 'myVar',
+                            arr, myArr
+                        },
+                        content: Array says {myVar}
+                    })
+                    ]
+                })`
+            }
+            </pre>
+            </code>
+            <br>
+            <h2>License</h2>
+            <br>
+            <p>This program is licensed under the ${getRandom(licenses)} license.</p> 
+            `
             document.getElementById('result').innerHTML = final;
         })
     
